ea4d37415f01dbd230fa3aef51bb0667f8186def
Hisashi Kamezawa <hkame6926@gmail.com>
2017-01-04 22:40:31 +0900

    fix reload bag

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
   post '/search_by_address' => 'home#search_by_address'
+  get '/search_by_address' => 'home#index'
   post '/search_by_current_location' => 'home#search_by_current_location'
+  get '/search_by_current_location' => 'home#index'
 end
